8a92f97eec9cced306b23c148b1dc223fe251b0f
hxp2023 <154068831+hxp2023@users.noreply.github.com>
2025-03-04 15:35:24 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false
+    },
+    watch: {
+      usePolling: false
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', '@mui/material']
+  }
 })
